cce89a41ce787e8bfef7da0ac96561adea678232
Yuto Saito <saito_y@tec.tomakomai.or.jp>
2018-07-05 10:23:38 +0900

    add function show_256_colors

--- .zsh/z1_functions.zsh
+++ .zsh/z1_functions.zsh
@@ -10,3 +10,12 @@ function wincmd(){
 function sqlext(){
 	cout | sed -e 's/[^"]*"\([^"]*\)".*/\1/' | cin && cout;
 }
+
+function show_256_colors(){
+	for c in {000..255};
+	do
+		echo -n "\e[38;5;${c}m $c";
+		[ $(($c%16)) -eq 15 ] && echo;
+	done;
+	echo
+}

90531a9c0edd928526c2ed09a05d23b670c9f28b
tuki9ko <arkmisha@gmail.com>
2018-08-20 11:32:45 +0900

    add comment, add func

--- .zsh/z1_functions.zsh
+++ .zsh/z1_functions.zsh
@@ -1,16 +1,20 @@
+# ヒストリ表示
 function history-all { history -E 1 }
 
+# MSYS2 使ってた時の名残
 function wincmd(){
 	CMD=$1
 	shift
 	$CMD $* 2>&1 | iconv -f CP932 -t UTF-8
 }
 
-
+# ソースコード中に書かれたSQLを抽出する
+# クリップボードで渡された文字列の中で '"' で囲まれた文字を抽出してクリップボードに上書きする
 function sqlext(){
 	cout | sed -e 's/[^"]*"\([^"]*\)".*/\1/' | cin && cout;
 }
 
+# peco でグラフィカルに cd 
 function lscd(){
 	if [ `which peco` ] ; then
 		local dir="$( find . -maxdepth 1 -type d | sed -e 's;\./;;' | peco )"
@@ -19,4 +23,31 @@ function lscd(){
 			lscd
 		fi
 	fi
+}
+
+# cd file で file のあるディレクトリに移動
+function cd(){
+	if [[ -e $1 && ! -d $1 ]]; then
+		builtin cd -- "$(dirname -- "$1")"
+	else
+		builtin cd "$@"
+	fi
+
+	local ret=$?
+	if [[ ($ret -eq 0) && (${#FUNCNAME[*]} -eq 1) ]]; then
+		history -s cd $(printf "%q" "$PWD")
+	fi
+	return $ret
+}
+
+# crontab -r を封印
+function crontab(){
+	local opt
+	for opt in "$@"; do
+		if [[ $opt == -r ]]; then
+			echo 'crontab -r is sealed!'
+			return 1
+		fi
+	done
+	command crontab "$@"
 }
